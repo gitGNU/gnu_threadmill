6fdfc83773192696556e8ae826f9443dd60d51d4
Banlu Kemiyatorn <object@gmail.com>
2011-01-11 22:02:36 +0700

    * Create dir.

--- build-go.sh
+++ build-go.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-(cd Threadmill;ln -sf ../TMLib/*.h ../TMKit/*.h .)
+(mkdir Threadmill;cd Threadmill;ln -sf ../TMLib/*.h ../TMKit/*.h .)
 export LD_LIBRARY_PATH="`pwd`"/TMLib/obj:"`pwd`"/TMKit/obj:$LD_LIBRARH_PATH
 make && ./Threadmill.app/Threadmill
